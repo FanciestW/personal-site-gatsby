54ff832a25d7504b4108df7e1fff9a8821f0ee59
William Lin <wlin26@yahoo.com>
2020-09-28 23:42:35 -0400

    Working on gatsby paging system

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React from "react";
+import Header from "../components/header";
 
-export default function Home() {
+export default function About() {
   return (
     <div style={{ color: `red` }}>
+      <Header headerText="Hello There!" />
       <h1>Hello my name is William!</h1>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby";
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <br></br>
+      <Link to="/contact/">Click me!</Link>
     </div>
   );
 }
