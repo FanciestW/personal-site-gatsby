35b1edf2cdc2d8e3022279c5bd32cbcf0d829710
William Lin <wlin26@yahoo.com>
2020-10-03 16:31:47 -0400

    Added typist to make text typing animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Box, Flex, Link, Text } from "rebass";
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css'
 
 import "../styles/header.scss";
 
 export default function Header(props) {
   return (
-    <Flex px={2} color="white" bg="black" alignItems="center">
-      <Text p={2} fontWeight="bold">
-        > Hello World
+    <Flex px={6} color="white" bg="black" alignItems="center">
+      <Text p={3} fontWeight="bold">
+        <Typist cursor={{blink:true}}>
+          > Hello World
+        </Typist>
       </Text>
       <Box mx="auto" />
-      <Link variant="nav" href="#!">
-        Profile
-      </Link>
     </Flex>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
 import React from "react"
-import { Link } from "gatsby";
+import Header from "../components/header";
 
 import "../styles/index.scss";
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-      <br></br>
-      <Link to="/contact/">Click me!</Link>
+    <div>
+      <Header />
     </div>
   );
 }
