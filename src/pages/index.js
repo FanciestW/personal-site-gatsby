4157f4bd70b46a7e05ddd49a9dd84a235d65b8f9
William Lin <wlin26@yahoo.com>
2020-10-04 19:43:19 -0400

    Added styling and content to build main index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from "react"
+import { Box, Flex, Text } from "rebass";
 import Header from "../components/header";
 
 import "../styles/index.scss";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Flex flexDirection='column' style={{height: '100vh'}}>
+      <Box>
+        <Header />
+      </Box>
+      <Flex flexGrow={1} px={6} style={{backgroundColor: '#272727'}}>
+        <Box p={3} style={{height: '100%'}}>
+          <Text>Hello World!</Text>
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
