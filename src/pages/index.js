a3ea858abc120e11349daff5be46919c5ecb08a1
William Lin <wlin26@yahoo.com>
2020-10-04 19:45:25 -0400

    Added body component that will have the content of the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <Header />
       </Box>
       <Flex flexGrow={1} px={6} style={{backgroundColor: '#272727'}}>
-        <Box p={3} style={{height: '100%'}}>
+        <Box p={3} style={{height: '100%', width: '100%', backgroundColor: '#0000002A'}}>
           <Text>Hello World!</Text>
         </Box>
       </Flex>
