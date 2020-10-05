8f2e87a354a12e4c697edbbaef4ca7ad3423fef8
William Lin <wlin26@yahoo.com>
2020-10-05 11:38:51 -0400

    Moved component code into each component

--- src/components/body.js
+++ src/components/body.js
@@ -1,7 +1,12 @@
+import React from "react"
+import { Box, Flex, Text } from "rebass";
+
 export default function Body(props) {
   return (
-    <div className='body'>
-      
-    </div>
+    <Flex flexGrow={1} px={[0, 6]} style={{ backgroundColor: "#272727" }}>
+      <Box p={3} style={{ height: "100%", width: "100%", backgroundColor: "#0000002A" }}>
+        <Text>Hello World!</Text>
+      </Box>
+    </Flex>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Box, Flex, Text } from "rebass";
+import { Box, Flex } from "rebass";
+import Body from "../components/body";
 import Header from "../components/header";
 
 import "../styles/index.scss";
@@ -7,14 +8,8 @@ import "../styles/index.scss";
 export default function Home() {
   return (
     <Flex color="white" bg="black" flexDirection='column' style={{height: '100vh'}}>
-      <Box>
-        <Header />
-      </Box>
-      <Flex flexGrow={1} px={[1,6]} style={{backgroundColor: '#272727'}}>
-        <Box p={3} style={{height: '100%', width: '100%', backgroundColor: '#0000002A'}}>
-          <Text>Hello World!</Text>
-        </Box>
-      </Flex>
+      <Header />
+      <Body />
     </Flex>
   );
 }
