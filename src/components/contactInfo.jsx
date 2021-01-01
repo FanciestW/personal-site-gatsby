df51e5ee85ebb85fbdf113ce052ebafadbc8a119
William Lin <wlin26@yahoo.com>
2021-01-01 14:57:30 -0500

    Changed contact link from Facebook to LinkedIn

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Box, Flex, Text, Link } from "rebass";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import ResumeDataContext from "./context/ResumeDataContext";
 
 export default function ContactInfo() {
+  const { email, profiles } = ResumeDataContext
+  let githubProfile, linkedinProfile, twitterProfile;
   return (
     <Box>
       <Link variant="contactIconLink">
@@ -15,7 +17,7 @@ export default function ContactInfo() {
         <FontAwesomeIcon size="2x" icon={faGithub} />
       </Link>
       <Link variant="contactIconLink">
-        <FontAwesomeIcon size="2x" icon={faFacebook} />
+        <FontAwesomeIcon size="2x" icon={faLinkedin} />
       </Link>
       <Link variant="contactIconLink">
         <FontAwesomeIcon size="2x" icon={faTwitter} />
