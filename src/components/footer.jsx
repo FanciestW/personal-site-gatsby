905f7833617881b9ce34b087c68593c0e8ab295e
William Lin <wlin26@yahoo.com>
2021-01-04 17:34:42 -0500

    Added two column footer content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Box, Flex, Text } from "rebass";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Flex
+      bg="primary"
+      variant="flexMargins"
+      alignItems="center"
+      py={[2, 3, 5]}
+      sx={{ display: "grid", gridGap: 4, gridTemplateColumns: "repeat(auto-fit, minmax(128px, 1fr))" }}>
+      <Box mx="auto">Col 1</Box>
+      <Box mx="auto">Col 2</Box>
+    </Flex>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "theme-ui";
 import { Flex } from "rebass";
 import Body from "../components/body";
 import Header from "../components/header";
+import Footer from "../components/footer";
 import ResumeDataContext from "../components/context/ResumeDataContext";
 
 import theme from "../themes";
@@ -16,6 +17,7 @@ export default function Home() {
         <ResumeDataContext.Provider value={ResumeDataJSON}>
           <Header />
           <Body />
+          <Footer />
         </ResumeDataContext.Provider>
       </Flex>
     </ThemeProvider>
